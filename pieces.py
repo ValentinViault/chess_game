3a477660f8028c9d75862a11bcf06d0915ec2fa8
ValentinViault <valentinviault1@gmail.com>
2026-03-20 01:35:09 +0100

    pieces.py

--- pieces.py
+++ pieces.py
@@ -11,29 +11,29 @@ class Piece:
 class Pion(Piece):
     def __init__(self, couleur):
         super().__init__(couleur)
-        self.symbole = "\u2659" if couleur == "blanc" else "\u265F" # Unicode du symbole de : Pion
+        self.symbole = "\u2659" if couleur == "blanc" else "\u265F" # Unicode du symbole de : Pion / Unicode for : Pawn
 
 class Fou(Piece):
     def __init__(self, couleur):
         super().__init__(couleur)
-        self.symbole = "\u2657" if couleur == "blanc" else "\u265D" # Unicode du symbole de : Fou
+        self.symbole = "\u2657" if couleur == "blanc" else "\u265D" # Unicode du symbole de : Fou  / Unicode for : Bishop
 
 class Cavalier(Piece):
     def __init__(self, couleur):
         super().__init__(couleur)
-        self.symbole = "\u2658" if couleur == "blanc" else "\u265E"  # Unicode du symbole de : Cavalier
+        self.symbole = "\u2658" if couleur == "blanc" else "\u265E"  # Unicode du symbole de : Cavalier / Unicode for : Knight
 
 class Tour(Piece):
     def __init__(self, couleur):
         super().__init__(couleur)
-        self.symbole = "\u2656" if couleur == "blanc" else "\u265C"  # Unicode du symbole de : Tour
+        self.symbole = "\u2656" if couleur == "blanc" else "\u265C"  # Unicode du symbole de : Tour / Unicode for : Rook (THE ROOOOOOOOOOOOKKKKKKKKKKKKKKKKKKKKKK)
 
 class Roi(Piece):
     def __init__(self, couleur):
         super().__init__(couleur)
-        self.symbole = "\u2654" if couleur == "blanc" else "\u265A"  # Unicode du symbole de : Roi
+        self.symbole = "\u2654" if couleur == "blanc" else "\u265A"  # Unicode du symbole de : Roi / Unicode for : King
 
 class Reine(Piece):
     def __init__(self, couleur):
         super().__init__(couleur)
-        self.symbole = "\u2655" if couleur == "blanc" else "\u265B"  # Unicode du symbole de : Reine
+        self.symbole = "\u2655" if couleur == "blanc" else "\u265B"  # Unicode du symbole de : Reine / Unicode for : Queen
